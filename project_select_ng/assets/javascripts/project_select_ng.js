551383c7527cb383876620616ad5f0c7278b6757
Florian Pommerening <admin@praedisoft.de>
2012-03-21 11:00:52 +0100

    removed redundant ";"

--- project_select_ng/assets/javascripts/project_select_ng.js
+++ project_select_ng/assets/javascripts/project_select_ng.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
     jQuery("#project-jump-id").change(function() {
-        window.location.href = "/projects/" + jQuery(this).val();;
+        window.location.href = "/projects/" + jQuery(this).val();
     });
 });
