dc75237862ac47a7cf78a2df3f796b8058098dca
Florian Pommerening <admin@praedisoft.de>
2012-04-05 12:53:10 +0200

    Added README and COPYRIGHT

--- init.rb
+++ init.rb
@@ -7,5 +7,5 @@ Redmine::Plugin.register :project_select_ng do
   author 'Florian Pommerening'
   description 'Shows a project search in a chosen-select field'
   version '0.0.1'
-  url 'https://bugtracking.kontextwork.de/issues/735'
+  url 'https://github.com/EugenMayer/projectselectng'
 end
