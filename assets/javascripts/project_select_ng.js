65522d33cd58b309ae4f317cbb82d95dc2ed22da
blackice2999 <blackice2999@googlemail.com>
2012-04-08 11:29:29 +0200

    fixes syntax error in js

--- assets/javascripts/project_select_ng.js
+++ assets/javascripts/project_select_ng.js
@@ -1,8 +1,7 @@
-;function($){
+;(function($){
   $(document).ready(function(){
       $("#project-jump-id").change(function() {
           window.location.href = $(this).val();
       });
-  });  
-}(jQuery);
-
+  }); 
+})(jQuery);
